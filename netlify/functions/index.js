9dfa6826b1aad5da0f103d79f765a66749bed079
Akhlaque <akhlaquea01@gmail.com>
2024-03-31 02:18:29 +0530

    file path issue fix

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,5 +1,5 @@
-import connectDatabase from "./src/config/db.js";
-import { launchBot } from "./src/telegrafBot.js";
+import connectDatabase from "../../src/config/db.js";
+import { launchBot } from "../../src/telegrafBot.js";
 
 async function startApplication() {
     try {
