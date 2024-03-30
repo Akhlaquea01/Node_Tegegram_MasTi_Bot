668761bc7d891d9d4a0f6e46bd0713c649dba756
Akhlaque <akhlaquea01@gmail.com>
2024-03-31 02:13:56 +0530

    build issue

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,7 +1,14 @@
-import connectDatabase from "../../src/config/db.js";
-import { launchBot } from "../../src/telegrafBot.js";
+import connectDatabase from "./src/config/db.js";
+import { launchBot } from "./src/telegrafBot.js";
 
-(async () => {
-    await connectDatabase();
-    await launchBot();
-})();
+async function startApplication() {
+    try {
+        await connectDatabase();
+        await launchBot();
+        console.log("Bot launched successfully!");
+    } catch (error) {
+        console.error("Error starting application:", error);
+    }
+}
+
+startApplication();
