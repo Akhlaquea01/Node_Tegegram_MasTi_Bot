dd19d92e5daec0cf60191b279564a7a24a17604d
Akhlaque <akhlaquea01@gmail.com>
2024-03-31 01:55:46 +0530

    netlify deploy changes

---
+++ netlify/functions/index.js
@@ -0,0 +1,7 @@
+import connectDatabase from "../../src/config/db.js";
+import { launchBot } from "../../src/telegrafBot.js";
+
+(async () => {
+    await connectDatabase();
+    await launchBot();
+})();
